8ef7720ced1ec3bf9dd21e541ed92c27f8c3a936
imlinus <imlinus@users.github.com>
2020-11-11 23:17:50 +0100

    ?

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+let plugins = [require('postcss-import'), require('autoprefixer')()]
+
+if (process.env.NODE_ENV == 'production') {
+  plugins.push(
+    require('cssnano')({
+      preset: 'default'
+    })
+  )
+}
+
+module.exports = {
+  plugins
+}
