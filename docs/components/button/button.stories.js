1ffa4064b02155e5c84e955f3f033c8be0836995
Linus Lilja <linus.lilja@qbank.se>
2020-11-11 08:21:49 +0100

    Added some more components

--- docs/components/button/button.stories.js
+++ docs/components/button/button.stories.js
@@ -1,11 +1,14 @@
-import "./button.css";
-import buttonText from "./button-text.html";
-import buttonLogo from "./button-logo.html";
+import './button.css'
+import buttonText from './button-text.html'
+import buttonLogo from './button-logo.html'
 
 export default {
-  title: "Components/Button",
-  parameters: { layout: "centered" },
-};
+  title: 'Components/Button',
+  parameters: {
+    layout: 'centered'
+  }
+}
 
-export const withText = () => buttonText;
-export const withLogo = () => buttonLogo;
+export const withText = () => buttonText
+export const withLogo = () => buttonLogo
+
